345e17d5d11ec51bb5b4a4f03e0c531c8c3d1f40
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2023-12-30 19:39:21 +0800

    to astro-icon v1 and use @iconify-json/simple-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import fs from "node:fs";
 import compress from "astro-compress";
+import icon from "astro-icon";
 const {
   homepage: domain
 } = JSON.parse(fs.readFileSync("./package.json"));
@@ -12,5 +13,5 @@ export default defineConfig({
   site: domain,
   integrations: [sitemap({
     filter: page => page !== `${domain}/404.html` && page !== `${domain}/404/` && page !== `${domain}/404`
-  }), compress()]
+  }), compress(), icon()]
 });
